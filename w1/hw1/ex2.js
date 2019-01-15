4c35bf5ab526bb8926e70d8162df8ac2b744bedf
Munkhbold Enkhtur <apple.muugii@gmail.com>
2019-01-15 00:08:00 -0600

    isWeekend implemented

---
+++ w1/hw1/ex2.js
@@ -0,0 +1,8 @@
+function isweekend() {
+  const todayDate = new Date()
+  const day = todayDate.getDay()
+  const days = ['weekend', 'weekday', 'weekday', 'weekday', 'weekday', 'weekday', 'weekend']
+  return days[day]
+}
+
+console.log(isweekend())
