55e8aaf3e4ef11f0a7ef9521adaefffe7729bd8e
Munkhbold Enkhtur <apple.muugii@gmail.com>
2019-01-15 13:57:37 -0600

    adding fold for exercise1

--- w1/hw1/ex1.js
+++ w1/hw1/ex1.js
@@ -1,6 +1,7 @@
 var {Observable, of} = require('rxjs');
 var {map:rxmap} = require('rxjs/operators');
 
+//:1 promise
 function promise(self, ban) {
     return new Promise((resolve, reject) => {
       if (true) {
@@ -11,6 +12,7 @@ function promise(self, ban) {
     })
 }
 
+//:1 async await
 async function asyncAwait(self, ban) {
     return await new Promise((resolve, reject) => {
       if (true) {
@@ -21,22 +23,26 @@ async function asyncAwait(self, ban) {
     })
 }
 
+//:1 filterWords
 String.prototype.filterWords = function(ban) {
   if (ban.includes('promise'))
-    return promise(this, ban)
+    promise(this, ban).then(data => console.log(data))
 
   if (ban.includes('async'))
-    return asyncAwait(this, ban)
+    asyncAwait(this, ban).then(data => console.log(data))
 
   if (ban.includes('observable')) {
-      return of(this.split(' ')).pipe(rxmap(word => ban.includes(word) ? '***': word))
+      of(this.split(' ')).pipe(
+        rxmap(word => ban.includes(word) ? '***': word)
+      ).subscribe(data => console.log(data))
   }
 
   return this.split(' ').map(word => ban.includes(word) ? '***' : word).join(' ')
 }
+// endfold
 
 let str = 'We should ban some words using ES6 features promise async and observable'
-str.filterWords(['promise']).then(value => console.log(value))
-str.filterWords(['async']).then(value => console.log(value))
-console.log(str.filterWords(['ES6', 'features']))
-str.filterWords(['observable']).subscribe(value => console.log(value))
+str.filterWords(['promise'])
+str.filterWords(['async'])
+str.filterWords(['ES6', 'features'])
+str.filterWords(['observable'])
