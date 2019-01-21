cb1e3557e1eed1fb39e5142060f6ba7f029079ae
Munkhbold Enkhtur <apple.muugii@gmail.com>
2019-01-21 15:33:18 -0600

    secret message is revealed

---
+++ w2/hw7/revealme.js
@@ -0,0 +1,33 @@
+const express = require('express')
+const mongo = require('mongodb').MongoClient
+const app = express()
+const client = mongo('mongodb://home:27017')
+
+const crypto = require('crypto')
+const algorithm = 'aes-256-cbc'
+const key = 'asaadsaad'
+
+//:1 Connection
+client.connect(err => {
+  const db = client.db('secret')
+  const collection = db.collection('reveal')
+  collection.insert({message: 'ba12e76147f0f251b3a2975f7acaf446a86be1b4e2a67a5d51d62f7bfbed5c03'})
+})
+
+
+//:1 Secret
+app.get('/secret', (req, res) => {
+  res.header('Content-Type', 'text/html; charset=utf-8')
+  const db = client.db('secret')
+  const collection = db.collection('reveal')
+
+  collection.findOne({}, (err, doc) => {
+    let decipher = crypto.createDecipher(algorithm, key);
+    let decrypted = decipher.update(doc.message, 'hex', 'utf-8');
+    decrypted += decipher.final('utf-8')
+    res.end(decrypted)
+  })
+
+})
+// endfold
+app.listen(8080, () => console.log('listening on 8080'))
