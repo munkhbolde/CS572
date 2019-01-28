57caff18d2fcf6af2cd86a45404f2a02c7862df4
Munkhbold Enkhtur <apple.muugii@gmail.com>
2019-01-28 15:15:16 -0600

    removing unnecesarry files

--- w3/hw11/src/app/app.component.ts
+++ w3/hw11/src/app/app.component.ts
@@ -6,7 +6,6 @@ import { Component } from '@angular/core';
 		<app-counter (counterChange)="dosomething($event)"></app-counter>
 		<span> ComponentCounterValue = {{ ComponentCounterValue }}</span>
 	`,
-  styleUrls: ['./app.component.css']
 })
 export class AppComponent {
   title = 'counter'
